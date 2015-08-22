c361e48ddb721c71ba6492920a89b67c8baf775c
Michal Zalecki <zalecki.post@gmail.com>
2015-08-22 10:40:17 +0200

    Send Access-Control-Allow-Origin=* by WebpackDevServer

--- server/webpack-dev-server.js
+++ server/webpack-dev-server.js
@@ -5,6 +5,7 @@ var config           = require('../webpack/webpack.dev.config');
 var webpackDevServer = new WebpackDevServer(webpack(config), {
   hot: true,
   historyApiFallback: true,
+  headers: { "Access-Control-Allow-Origin": "*" },
 });
 
 webpackDevServer.listen(9090,'localhost', function (err) {
