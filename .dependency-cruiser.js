92f88fd715e8d40d570ce9f98c79ac61ca745443
Michal Zalecki <michal@michalzalecki.com>
2019-09-18 12:03:51 +0200

    Update codechecks config

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -14,7 +14,7 @@ module.exports = {
         },
         {
             name: 'no-orphans',
-            severity: 'warn',
+            severity: 'error',
             comment:
                 "This is an orphan module - it's likely not used (anymore?). Either use it or remove it. If it's " +
                 "logical this module is an orphan (i.e. it's a config file), add an exception for it in your " +
